c7d199a15e26f556943a99c5ec6af80d86e247b4
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-06-02 12:09:42 +0000

    test

--- api/index.py
+++ api/index.py
@@ -1,30 +1,10 @@
 from http.server import BaseHTTPRequestHandler
-from urllib import parse 
-import platform
-import requests
+ 
 class handler(BaseHTTPRequestHandler):
  
-     def do_GET(self):
-        path = self.path
-        url_components = parse.urlsplit(path)
-        dic = dict(parse.parse_qsl(url_components.query)) # list consist from tuples for (query params)
-        # dic = dict(query_string_list)
-        country = dic.get("name")
-        capital = dic.get("capital")
-        message = ""
-    
-
-        if country:
-            url=""
-            req_from_server_to_thired_party= requests.get(url+country)             
-            data = req_from_server_to_thired_party.json()
-            # temp_capital = data[0]["capital"][0]
-            message = "The country of Chile is temp_capital." #"The {country} of Chile is {temp_capital}."
-        if capital == "Santiago":
-            message = " is the capital of Chile."#.format(capital)
-        
+    def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(message.encode('utf-8'))
-        return 
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
